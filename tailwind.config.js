18484b9ea3062960ed5e8b532a5df1da1740a67d
Mariya Zaharieva <mariqtrunkova@gmail.com>
2024-05-28 22:14:50 +0300

    Add fonts, colors and images

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,17 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+        'source-sans': ['Source Sans Pro', 'sans-serif'],
+      },
+      color: {
+        'green-light': '#B4FF5C',
+        'gray-text': '#606060',
+        'gray-light': '#F1F4F5'
+      }
+    },
   },
   plugins: [],
 }
