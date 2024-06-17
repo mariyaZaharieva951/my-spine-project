6ef796acc8ff4e93020d160670803f400941ce10
Mariya Zaharieva <mariqtrunkova@gmail.com>
2024-06-17 23:34:34 +0300

    Add OurStory component

---
+++ src/app/about/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+import About from "@/components/layouts/About";
+import Footer from "@/components/layouts/Footer";
+import Header from "@/components/layouts/Header";
+import OurStory from "@/components/layouts/OurStory";
+
+const AboutUs = () => {
+  return (
+   <div>
+        <Header/>
+        <About/>
+        <OurStory/>
+        <Footer/>
+   </div>
+  );
+};
+
+export default AboutUs;
